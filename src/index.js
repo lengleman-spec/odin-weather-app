61efe2af2a3b6d622281365408c0ec575c911085
lengleman-spec <191269315+lengleman-spec@users.noreply.github.com>
2026-03-13 16:26:03 -0400

    Add logic to fetch the weather. And variables to store it.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import "./style.css";
 
-console.log("Webpack is working!");
+const apiKey = "c588243f8658e3f6dcd9287218bf1708";
+const baseUrl = "https://api.openweathermap.org/data/2.5/weather";
+
+async function fetchWeather(location) {
+  try {
+    const response = await fetch(
+      `${baseUrl}?q=${location}&appid=${apiKey}&units=metric`,
+    );
+
+    if (!response.ok) throw new Error("Location not found");
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+fetchWeather("London");
