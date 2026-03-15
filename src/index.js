9fa10d990341037d8ff254ce1d8cdc4a5c1eb71b
lengleman-spec <191269315+lengleman-spec@users.noreply.github.com>
2026-03-15 11:33:00 -0400

    Write logic to only use properties that are necessary - remove giant API object.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import "./style.css";
 const apiKey = "c588243f8658e3f6dcd9287218bf1708";
 const baseUrl = "https://api.openweathermap.org/data/2.5/weather";
 
+function processWeatherData(data) {
+  return {
+    city: data.name,
+    temperature: data.main.temp,
+    feelsLike: data.main.feels_like,
+    humidity: data.main.humidity,
+    description: data.weather[0].description,
+  };
+}
+
 async function fetchWeather(location) {
   try {
     const response = await fetch(
@@ -11,8 +21,11 @@ async function fetchWeather(location) {
 
     if (!response.ok) throw new Error("Location not found");
     const data = await response.json();
-    console.log(data);
-    return data;
+
+    const weather = processWeatherData(data);
+
+    console.log(weather);
+    return weather;
   } catch (error) {
     console.error(error);
     return null;
